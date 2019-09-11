31ceaed1ce5d6956b1ded4d8fc85c483172133d8
Kenya-a <Kenya-a@users.noreply.github.com>
2019-09-11 15:53:41 -0500

    models

--- src/main/java/com/lambdaschool/javaorders/models/Customer.java
+++ src/main/java/com/lambdaschool/javaorders/models/Customer.java
@@ -24,6 +24,148 @@ public class Customer
     private String phone;
 
     //many to one
+    @ManyToOne
+    @JoinColumn(name = "agentcode", nullable = false)
+    private Agent agent;
 
+    //constructor
+    public Customer()
+    {
+    }
 
+    public Customer(String custname, String custcity, String workingarea, String custcountry, String grade, double openingamt, double receiveamt, double paymentamt, double oustandingamt, String phone, Agent agent)
+    {
+        this.custname = custname;
+        this.custcity = custcity;
+        this.workingarea = workingarea;
+        this.custcountry = custcountry;
+        this.grade = grade;
+        this.openingamt = openingamt;
+        this.receiveamt = receiveamt;
+        this.paymentamt = paymentamt;
+        this.oustandingamt = oustandingamt;
+        this.phone = phone;
+        this.agent = agent;
+    }
+
+    //getter and setter
+    public long getCustcode()
+    {
+        return custcode;
+    }
+
+    public void setCustcode(long custcode)
+    {
+        this.custcode = custcode;
+    }
+
+    public String getCustname()
+    {
+        return custname;
+    }
+
+    public void setCustname(String custname)
+    {
+        this.custname = custname;
+    }
+
+    public String getCustcity()
+    {
+        return custcity;
+    }
+
+    public void setCustcity(String custcity)
+    {
+        this.custcity = custcity;
+    }
+
+    public String getWorkingarea()
+    {
+        return workingarea;
+    }
+
+    public void setWorkingarea(String workingarea)
+    {
+        this.workingarea = workingarea;
+    }
+
+    public String getCustcountry()
+    {
+        return custcountry;
+    }
+
+    public void setCustcountry(String custcountry)
+    {
+        this.custcountry = custcountry;
+    }
+
+    public String getGrade()
+    {
+        return grade;
+    }
+
+    public void setGrade(String grade)
+    {
+        this.grade = grade;
+    }
+
+    public double getOpeningamt()
+    {
+        return openingamt;
+    }
+
+    public void setOpeningamt(double openingamt)
+    {
+        this.openingamt = openingamt;
+    }
+
+    public double getReceiveamt()
+    {
+        return receiveamt;
+    }
+
+    public void setReceiveamt(double receiveamt)
+    {
+        this.receiveamt = receiveamt;
+    }
+
+    public double getPaymentamt()
+    {
+        return paymentamt;
+    }
+
+    public void setPaymentamt(double paymentamt)
+    {
+        this.paymentamt = paymentamt;
+    }
+
+    public double getOustandingamt()
+    {
+        return oustandingamt;
+    }
+
+    public void setOustandingamt(double oustandingamt)
+    {
+        this.oustandingamt = oustandingamt;
+    }
+
+    public String getPhone()
+    {
+        return phone;
+    }
+
+    public void setPhone(String phone)
+    {
+        this.phone = phone;
+    }
+
+    public Agent getAgent()
+    {
+        return agent;
+    }
+
+    public void setAgent(Agent agent)
+    {
+        this.agent = agent;
+    }
 }
---
+++ src/main/java/com/lambdaschool/javaorders/repos/CustomerRepository.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.javaorders.repos;
+
+public interface CustomerRepository
+{
+}
